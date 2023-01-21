b33172f3f6beb8838ea02be718967ce55ec7dbcd
Fernanda Santos <santos4fer@gmail.com>
2023-01-21 20:44:55 -0300

    breaking change: guardando dados (#4)
    Co-authored-by: Fernanda Santos <santo4fer@gmail.com>

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
-    run: ['01-16','01-17','01-18','01-19','01-20'],
-    food: ['01-16','01-17','01-18'],
-    water: ['01-16'],
-    journal: [],
-    meditate: ['01-16','01-17','01-18']
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert('Dia já incluso')
+        return
+    }
+
+    nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))    
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
